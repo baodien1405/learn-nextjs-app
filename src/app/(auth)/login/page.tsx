aa8c2a19afaf68fdbd735bb3ed21c7a1ac10ba5c
Dien Cap Bao <dien.capbao@sharpensols.com>
2024-05-22 15:10:34 +0700

    feat: Add dark/light mode with shadcn-ui

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from '@/components/mode-toggle'
+
+export default function LoginPage() {
+  return (
+    <div>
+      <ModeToggle />
+      <h1>LoginPage</h1>
+    </div>
+  )
+}
